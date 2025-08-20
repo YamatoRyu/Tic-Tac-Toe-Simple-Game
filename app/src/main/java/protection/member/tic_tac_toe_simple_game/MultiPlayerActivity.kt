0a9805b9bc81fe198a62bdc24c32997d0fcdd5e6
YamatoRyuma <gamerliu2004@gmail.com>
2025-08-20 10:01:49 +0700

    The Simple Game part 1

--- app/src/main/java/protection/member/tic_tac_toe_simple_game/MultiPlayerActivity.kt
+++ app/src/main/java/protection/member/tic_tac_toe_simple_game/MultiPlayerActivity.kt
@@ -1,44 +1,47 @@
 package protection.member.tic_tac_toe_simple_game
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.material.button.MaterialButton
 import protection.member.tic_tac_toe_simple_game.databinding.ActivityMultiPlayerBinding
 
 class MultiPlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMultiPlayerBinding
+
+    private var firstPlayer = 0
+    private var secondPlayer = 0
+
     enum class Turn {
         NOUGHT,
         CROSS
     }
 
     companion object {
-        const val FIRST_PLAYER = "O"
+        const val FIRST_PLAYER_TEXT = "O"
+        const val SECOND_PLAYER_TEXT = "X"
+
         val FIRST_PLAYER_COLOR = R.color.you_chan
-        const val SECOND_PLAYER = "X"
         val SECOND_PLAYER_COLOR = R.color.red
     }
 
     private var finishTurn = Turn.CROSS
     private var currentTurn = Turn.CROSS
-    private val boardList = mutableListOf<MaterialButton>()
-
-    private lateinit var binding: ActivityMultiPlayerBinding
-
-    private lateinit var scorePlayer1: TextView
-    private lateinit var scorePlayer2: TextView
+    private val arrayButtonTable = mutableListOf<MaterialButton>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMultiPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        playGame()
-        scorePlayer1 = binding.player1Score
-        scorePlayer2 = binding.player2Score
+        binding.player1Score.text = firstPlayer.toString()
+        binding.player2Score.text = secondPlayer.toString()
+
+        materialButtonAdd()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -55,58 +58,60 @@ class MultiPlayerActivity : AppCompatActivity() {
         return false
     }
 
-    private fun playGame() {
-        boardList.add(binding.button1)
-        boardList.add(binding.button2)
-        boardList.add(binding.button3)
-        boardList.add(binding.button4)
-        boardList.add(binding.button5)
-        boardList.add(binding.button6)
-        boardList.add(binding.button7)
-        boardList.add(binding.button8)
-        boardList.add(binding.button9)
+    private fun materialButtonAdd() {
+        arrayButtonTable.add(binding.button1)
+        arrayButtonTable.add(binding.button2)
+        arrayButtonTable.add(binding.button3)
+        arrayButtonTable.add(binding.button4)
+        arrayButtonTable.add(binding.button5)
+        arrayButtonTable.add(binding.button6)
+        arrayButtonTable.add(binding.button7)
+        arrayButtonTable.add(binding.button8)
+        arrayButtonTable.add(binding.button9)
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun addMaterialButton(materialButton: MaterialButton) {
+        if (materialButton.text != "") return
+
+        if (currentTurn == Turn.CROSS) {
+            materialButton.text = "O"
+            materialButton.setTextColor(ContextCompat.getColor(this, R.color.you_chan))
+
+            currentTurn = Turn.NOUGHT
+        }
+
+        else if (currentTurn == Turn.NOUGHT) {
+            materialButton.text = "X"
+            materialButton.setTextColor(Color.RED)
+
+            currentTurn = Turn.CROSS
+        }
+
+        getTurn()
+    }
+
+    private fun getTurn() {
+        var turnText = ""
+        var turnColor = 0
+
+        if (currentTurn == Turn.CROSS) {
+            turnText = "Turn $FIRST_PLAYER_TEXT"
+            turnColor = FIRST_PLAYER_COLOR
+        }
+
+        else if (currentTurn == Turn.NOUGHT) {
+            turnText = "Turn $SECOND_PLAYER_TEXT"
+            turnColor = SECOND_PLAYER_COLOR
+        }
+
+        binding.turn.text = turnText
+        binding.turn.setTextColor(ContextCompat.getColor(this, turnColor))
     }
 
     fun onClickButton(view: View) {
         if (view !is MaterialButton) return
 
-        addToBoard(view)
-    }
-
-    private fun addToBoard(materialButton: MaterialButton) {
-        if (materialButton.text != "") return
-
-        if (currentTurn == Turn.NOUGHT) {
-            materialButton.text = FIRST_PLAYER
-            materialButton.setTextColor(FIRST_PLAYER_COLOR)
-            currentTurn = Turn.CROSS
-        }
-
-        else if (currentTurn == Turn.CROSS) {
-            materialButton.text = SECOND_PLAYER
-            materialButton.setTextColor(SECOND_PLAYER_COLOR)
-            currentTurn = Turn.NOUGHT
-        }
-
-        setTurnLabel()
-    }
-
-    @SuppressLint("ResourceAsColor")
-    private fun setTurnLabel() {
-        var turnText = ""
-        var turnColor = 0
-
-        if (currentTurn == Turn.CROSS) {
-            turnText = "Turn $SECOND_PLAYER"
-            turnColor = R.color.you_chan
-        }
-
-        else if (currentTurn == Turn.NOUGHT) {
-            turnText = "Turn $FIRST_PLAYER"
-            turnColor = R.color.red
-        }
-
-        binding.turn.text = turnText
-        binding.turn.setTextColor(turnColor)
+        addMaterialButton(view)
     }
 }
