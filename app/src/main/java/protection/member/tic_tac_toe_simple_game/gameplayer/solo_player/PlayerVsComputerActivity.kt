c71626bf65f653c5bc3f21ef9f316ff05931aaa7
YamatoRyuma <gamerliu2004@gmail.com>
2025-08-22 14:56:35 +0700

    Simple Game part 2

--- app/src/main/java/protection/member/tic_tac_toe_simple_game/gameplayer/solo_player/PlayerVsComputerActivity.kt
+++ app/src/main/java/protection/member/tic_tac_toe_simple_game/gameplayer/solo_player/PlayerVsComputerActivity.kt
@@ -1,12 +1,42 @@
 package protection.member.tic_tac_toe_simple_game.gameplayer.solo_player
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 import protection.member.tic_tac_toe_simple_game.R
+import protection.member.tic_tac_toe_simple_game.databinding.ActivityPlayerVsComputerBinding
 
 class PlayerVsComputerActivity : AppCompatActivity() {
+    private var _binding: ActivityPlayerVsComputerBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val PLAYER = "O"
+        const val COMPUTER = "X"
+    }
+
+    private val array = mutableListOf<MaterialButton>()
+
+    private val firstTurn = SoloPlayerTurn.PLAYER_CIRCLE
+    private val drawFirstTurn = SoloPlayerTurn.PLAYER_CIRCLE
+
+    private lateinit var player: TextView
+    private lateinit var computer: TextView
+
+    private var playerScore = 0
+    private var computerScore = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player_vs_computer)
+        _binding = ActivityPlayerVsComputerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onSupportNavigateUp()
+        return true
     }
 }
---
+++ app/src/main/java/protection/member/tic_tac_toe_simple_game/gameplayer/solo_player/SoloPlayerTurn.kt
@@ -0,0 +1,6 @@
+package protection.member.tic_tac_toe_simple_game.gameplayer.solo_player
+
+enum class SoloPlayerTurn {
+    PLAYER_CIRCLE,
+    COMPUTER_CROSS
+}
